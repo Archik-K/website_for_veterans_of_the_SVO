5b8164e311fd3fbd67a7230d19fa856de3ae602e
Archik-K <ar.kryatova@yandex.ru>
2025-03-16 16:56:18 +0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
 import styles from "../assets/styles/main.module.css"
-
+import LogoSVO from "../assets/logo/Logo_SVO_color.svg"
+import VK from "../assets/icon/vk.svg"
+import TG from "../assets/icon/tg.svg"
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className="header__logo">Логотип</div>
-      <nav className="header__nav">
-        <a href="/" className="header__link">О нас</a>
-        <a href="/about" className="header__link">Проекты</a>
-        <a href="/contact" className="header__link">Контакты</a>
-        <a href="/contact" className="header__link">Обратная связь</a>
+      <div className={styles.header__logo}><img src={LogoSVO} alt="Логотип сайта" /></div>
+      <nav className={styles.header__nav}>
+        <a href="/" className={styles.header__link}>О нас</a>
+        <a href="/" className={styles.header__link}>Проекты</a>
+        <a href="/" className={styles.header__link}>Контакты</a>
+        <a href="/" className={styles.header__link}>Обратная связь</a>
+        <a href="https://vk.com/association_svo_vrn" className={styles.header__link}><img src={VK} alt="Ссылка на вконтакте" /></a>
+        <a href="https://t.me/avsvo36" className={styles.header__link}><img src={TG} alt="Ссылка на телеграм" /></a>
       </nav>
     </header>
   );
